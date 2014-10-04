4b7a3da69d684aa82a58fbde043d5a1cb52d5db2
frehner <frehnar@hotmail.com>
2014-10-04 10:53:14 -0600

    The tab bar is in, in addition to a complete copy of the storyboard for the set game and all the outlets are still connected.  Now to start modifying the controller behind it.

---
+++ Matchismo1/SetGameViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SetGameViewController.swift
+//  Matchismo1
+//
+//  Created by Anthony Frehner on 10/4/14.
+//  Copyright (c) 2014 IS 543. All rights reserved.
+//
+
+import UIKit
+
+class SetGameViewController : CardGameViewController {
+
+}
